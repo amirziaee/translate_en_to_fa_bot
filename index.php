98e6d058f56a71b2215e549c047c405dd0069853
amirziaee <amir.fr2013@yahoo.com>
2023-09-19 14:36:49 +0330

    syntax error fixed

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ use A14e\Translate\TranslateApi\GoogleApi;
 
 require './vendor/autoload.php';
 
-$hello = 
+
 $data = file_get_contents('php://input');
 $dataJson = json_decode($data);
 $entities = $dataJson->message->entities[0] ?? null;
@@ -16,7 +16,7 @@ $token = '6459149107:AAEjzm2Qx5B7ljyksV5hEoLhOxC4XeOJitA';
 
 $translated = ((new GoogleApi())->choosenApi())->getTranslatedData($dataJson->message->text);
 $bot = new Bot($token,$dataJson->message->chat->id);
-$helloMessage = 'سلام'.$dataJson->message->chat->first_name.'!'.PHP_EOL.'برای اینکه کلمه یا متن رو ترجمه کنی او کلمه یا متن رو برامون بنویس.';
+$helloMessage = ' سلام'.$dataJson->message->chat->first_name.'!'.PHP_EOL.'برای اینکه کلمه یا متن رو ترجمه کنی اون کلمه یا متن رو برامون بنویس.';
 if($dataJson->message->text == '/start' and $entities->type == 'bot_command')
 {
     $bot->sendMessage($helloMessage);
